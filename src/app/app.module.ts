cda46ba8894c5600cf4da8eab3a7c9334970fa52
rdlopes17 <rauldlopes@gmail.com>
2024-11-04 08:43:21 -0300

    desenv ngModel pt6-criando directiva para controle de idade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CadastroComponent } from "./cadastro/cadastro.component";
 import { SucessoCadastroComponent } from "./sucesso-cadastro/sucesso-cadastro.component";
 import { FormsModule } from "@angular/forms";
 import { MensagemComponent } from './components/mensagem/mensagem.component';
+import { MaiorIdadeDirective } from './directives/maior-idade.directive';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { MensagemComponent } from './components/mensagem/mensagem.component';
     CadastroComponent,
     SucessoCadastroComponent,
     MensagemComponent,
+    MaiorIdadeDirective,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
